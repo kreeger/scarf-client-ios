a0a166d25ebf3c4c47ed6ad34af7bbbd6102f678
Ben Kreeger <ben@kree.gr>
2013-08-27 07:35:34 -0500

    Adding initial files.

---
+++ Classes/Core/BDKAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  BDKAppDelegate.h
+//  Scarf
+//
+//  Created by Ben Kreeger on 8/27/13.
+//  Copyright (c) 2013 Ben Kreeger. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BDKAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Classes/Core/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Scarf
+//
+//  Created by Ben Kreeger on 8/27/13.
+//  Copyright (c) 2013 Ben Kreeger. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "BDKAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([BDKAppDelegate class]));
+    }
+}
